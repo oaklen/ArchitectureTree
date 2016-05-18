d115be33e22c4b2b7eeed0f9b01f9daa1cbac929
oaklen <florhwu@gmail.com>
2016-05-18 08:23:13 +0800

    architectureTree test

--- js/d3.architectureTree.js
+++ js/d3.architectureTree.js
@@ -4,26 +4,36 @@ d3.chart = d3.chart || {};
 
 d3.chart.architectureTree = function() {
 
-    var svg, tree, treeData, diameter, activeNode;
+    var svg, tree, treeData, diameter, activeNode, diagonal;
+
+    var margin = {top: 20, right: 120, bottom: 20, left: 120},
+        width = 960 - margin.right - margin.left,
+        height = 500 - margin.top - margin.bottom;
+
+    var x0 = height / 2;
+    var y0 = 0;
 
     /**
      * Build the chart
      */
     function chart(){
-        if (typeof(tree) === 'undefined') {
-            tree = d3.layout.tree()
-                .size([360, diameter / 2 - 120])
-                .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
-            svg = d3.select("#graph").append("svg")
-                .attr("width", diameter)
-                .attr("height", diameter )
-                .append("g")
-                .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
-        }
+        tree = d3.layout.tree()
+            .size([height, width]);
 
-        var nodes = tree.nodes(treeData),
+        svg = d3.select("body").append("svg")
+            .attr("width", width + margin.right + margin.left)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.select(self.frameElement).style("height", "500px");
+        
+        var nodes = tree.nodes(treeData).reverse(),
             links = tree.links(nodes);
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function(d) { d.y = d.depth * 180; });
 
         activeNode = null;
 
@@ -43,8 +53,8 @@ d3.chart.architectureTree = function() {
             addIndex(node);
         });
 
-        var diagonal = d3.svg.diagonal.radial()
-            .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+        var diagonal = d3.svg.diagonal()
+            .projection(function(d) { return [d.y, d.x]; });
 
         var linkSelection = svg.selectAll(".link").data(links, function(d) {
             return d.source.name + d.target.name + Math.random();
@@ -62,7 +72,7 @@ d3.chart.architectureTree = function() {
 
         var node = nodeSelection.enter().append("g")
             .attr("class", "node")
-            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
             .on('mouseover', function(d) {
                 if(activeNode !== null) {
                     return;
@@ -83,7 +93,7 @@ d3.chart.architectureTree = function() {
             });
 
         node.append("circle")
-            .attr("r", function(d) { return 4.5 * (d.size || 1); })
+            .attr("r", 1e-6)
             .style('stroke', function(d) {
                 return d3.scale.linear()
                     .domain([1, 0])
@@ -96,9 +106,11 @@ d3.chart.architectureTree = function() {
                     .range(["white", "#f66"])(typeof d.satisfaction !== "undefined" ? d.satisfaction : 1);
             });
 
+        //add text
         node.append("text")
-            .attr("dy", ".31em")
-            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+            // .attr("x", function(d) { return d.children || d._children ? -13 : 13; }) 
+            .attr("dy", ".35em")
+            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end";  })
             .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
             .text(function(d) {
                 return d.name;
@@ -133,13 +145,6 @@ d3.chart.architectureTree = function() {
      *
      * Mutates the given node (datum).
      *
-     * Example added properties:
-     * {
-     *   index: {
-     *     relatedNodes: ["Foo", "Bar", "Baz", "Buzz"],
-     *     technos: ["Foo", "Bar"],
-     *     host: ["OVH", "fo"] 
-     *   }
      * }
      */
     var addIndex = function(node) {
@@ -205,9 +210,9 @@ d3.chart.architectureTree = function() {
     };
 
     var filters = {
-      name: '',
-      technos: [],
-      hosts: []
+        name: '',
+        technos: [],
+        hosts: []
     };
 
     var isFoundByFilter = function(d) {
